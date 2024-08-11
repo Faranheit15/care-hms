39907105af41cfbd0c1d2a4e2b150bd4a4c95a05
Faranheit15 <ffaranm15@gmail.com>
2024-08-11 18:43:34 +0530

    removed phone number validation

--- lib/validation.ts
+++ lib/validation.ts
@@ -6,11 +6,5 @@ export const UserFormValidation = z.object({
     .min(2, "Name must be at least 2 characters.")
     .max(50, "Name must be less than 50 characters."),
   email: z.string().email("Invalid email address."),
-  phone: z
-    .string()
-    .refine(
-      (phone) =>
-        /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(phone),
-      "Invalid phone number"
-    ),
+  phone: z.string(),
 });
